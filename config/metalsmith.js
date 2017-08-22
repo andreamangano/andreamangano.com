8b7e63bc7d6b3b132550a434cb2e18025bb9f6de
Andrea Mangano <andrea.mangano@ibuildings.it>
2017-08-22 16:38:02 +0200

    Added config files

---
+++ config/metalsmith.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+module.exports = {
+  sourceDir: './src/',
+  destinationDir: './build/',
+  templateDir: './templates/'
+};
--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+const devBuild = process.env.NODE_ENV === 'development';
+
 const Metalsmith = require('metalsmith'),
   layouts = require('metalsmith-layouts'),
-  markdown = require('metalsmith-markdown');
+  markdown = require('metalsmith-markdown'),
+  pkg = require('./package.json'),
+  config = require('./config/metalsmith');
+
+console.log(`${devBuild ? 'Development' : 'Production'} build, version ${pkg.version}`);
 
 const builder = Metalsmith(__dirname)
-  .source('./src')
-  .destination('./build')
+  .source(config.sourceDir)
+  .destination(config.destinationDir)
   .clean(true)
   .use(markdown())
   .use(layouts({
     engine: 'pug',
-    directory: './templates'
+    directory: config.templateDir
   }))
   .build(function (err) {
     if (err) throw err;

f1d4932ebff2ac393130ccdbe2da6a06e015e526
Andrea Mangano <andrea.mangano@ibuildings.it>
2017-08-22 17:00:33 +0200

    Refining

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -4,10 +4,10 @@ yarn install
 yarn run prod
 cd build
 rm -rf .git/
-git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
-git config --global user.name "$GH_NAME" > /dev/null 2>&1
+git config --global user.email "$GH_EMAIL"
+git config --global user.name "$GH_NAME"
 git init
 git remote add origin $GITURL
 git add .
-git commit -am "deploy"
+git commit -am "Automated Rebuild"
 git push origin master:gh-pages --force
